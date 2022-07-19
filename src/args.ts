65d5a96461e47bb75b8a03d4f75e76d1396295e5
Simon <sstur@me.com>
2022-07-19 14:15:40 -0700

    New approach with arg() and flag(); no name generic

--- src/args.ts
+++ src/args.ts
@@ -1,70 +1,56 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-type ArgType = 'flag' | 'arg';
-
-type ArgDefInput<T extends ArgType, M extends boolean> = {
+type ArgDef<M extends boolean> = {
+  type: 'arg';
   alias?: string;
-  typeLabel?: string;
-  description: string;
-  type?: T;
-  multiple?: M;
-};
-
-type ArgDef<N extends string, T extends ArgType, M extends boolean> = {
-  name: N;
-  alias: string;
   typeLabel: string;
   description: string;
-  type: T;
   multiple: M;
 };
 
-function defineSchema<
-  O extends Record<string, (name: string) => ArgDef<string, ArgType, boolean>>,
->(
-  definer: (arg: ArgDefiner) => O,
-): { [K in keyof O]: Expand<{ name: K } & Omit<ReturnType<O[K]>, 'name'>> } {
-  const object = definer(arg);
-  return Object.fromEntries(
-    Object.entries(object).map(
-      ([name, getter]) => [name, getter(name)] as const,
-    ),
-  ) as any;
-}
+type ArgDefInput<M extends boolean> = Expand<
+  Omit<ArgDef<M>, 'type' | 'multiple'> & { multiple?: M }
+>;
 
-type WithDefaults<I extends ArgDefInput<ArgType, boolean>> =
-  I extends ArgDefInput<infer T, infer M>
-    ? {
-        alias: string;
-        typeLabel: string;
-        description: string;
-        type: ArgType extends T ? 'arg' : T;
-        multiple: [M] extends [true] ? true : false;
-      }
-    : never;
+type FlagDef = {
+  type: 'flag';
+  alias?: string;
+  description: string;
+};
 
-function withDefaults<I extends ArgDefInput<ArgType, boolean>>(
-  input: I,
-): WithDefaults<I> {
-  const { alias, typeLabel, description, type, multiple } = input;
-  return {
-    alias: alias ?? '_',
-    typeLabel: typeLabel ?? '',
-    description,
-    type: type ?? 'arg',
-    multiple: multiple ?? false,
-  } as any;
-}
+type FlagDefInput = Omit<FlagDef, 'type'>;
 
-function arg<T extends ArgType, M extends boolean, I extends ArgDefInput<T, M>>(
-  input: I,
+type OptionDef<M extends boolean> = ArgDef<M> | FlagDef;
+
+type Definers = {
+  arg: ArgDefiner;
+  flag: FlagDefiner;
+};
+
+function defineSchema<O extends Record<string, OptionDef<boolean>>>(
+  getSchema: (definers: Definers) => O,
 ) {
-  return <N extends string>(name: N): Expand<{ name: N } & WithDefaults<I>> =>
-    ({ name, ...withDefaults(input) } as any);
+  return getSchema({ arg, flag });
+}
+
+function arg<M extends boolean>(
+  input: ArgDefInput<M>,
+): [M] extends [true] ? ArgDef<true> : ArgDef<false> {
+  const { multiple, ...rest } = input;
+  const result = multiple
+    ? { type: 'arg', multiple: true, ...rest }
+    : { type: 'arg', multiple: false, ...rest };
+  return result as any;
 }
 
 type ArgDefiner = typeof arg;
 
-const _result = defineSchema((arg) => ({
-  foo: arg({ alias: 'f', description: 'Foo' }),
-  bar: arg({ alias: 'b', description: 'Bar' }),
+function flag(input: Expand<FlagDefInput>): FlagDef {
+  return { type: 'flag', ...input };
+}
+
+type FlagDefiner = typeof flag;
+
+const _result = defineSchema(({ arg, flag }) => ({
+  foo: arg({ alias: 'f', typeLabel: '<foo>', description: 'Foo' }),
+  bar: flag({ alias: 'b', description: 'Bar' }),
 }));
