967f98ec4be6a4f3a9c50bf8444198ac6ce2c55e
Simon <sstur@me.com>
2022-07-19 17:37:26 -0700

    Implement optional validator

--- src/args.ts
+++ src/args.ts
@@ -1,20 +1,29 @@
-type ArgDef = {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/** If T is a function, get the return type, else fall back to U */
+type ReturnTypeOr<T, U> = T extends (...args: Array<any>) => any
+  ? ReturnType<T>
+  : U;
+
+type ArgDef<T> = {
   type: 'arg';
   alias?: string;
   typeLabel: string;
   description: string;
+  validator?: (input: string) => T;
 };
 
-type ArgDefInput = Omit<ArgDef, 'type'>;
+type ArgDefInput<T> = Omit<ArgDef<T>, 'type'>;
 
-type ArgListDef = {
+type ArgListDef<T> = {
   type: 'argList';
   alias?: string;
   typeLabel: string;
   description: string;
+  validator?: (input: string) => T;
 };
 
-type ArgListDefInput = Omit<ArgListDef, 'type'>;
+type ArgListDefInput<T> = Omit<ArgListDef<T>, 'type'>;
 
 type FlagDef = {
   type: 'flag';
@@ -24,7 +33,7 @@ type FlagDef = {
 
 type FlagDefInput = Omit<FlagDef, 'type'>;
 
-type OptionDef = ArgDef | ArgListDef | FlagDef;
+type OptionDef<T> = ArgDef<T> | ArgListDef<T> | FlagDef;
 
 type Definers = {
   arg: ArgDefiner;
@@ -32,20 +41,28 @@ type Definers = {
   flag: FlagDefiner;
 };
 
-function defineSchema<O extends Record<string, OptionDef>>(
+function defineSchema<O extends Record<string, OptionDef<unknown>>>(
   getSchema: (definers: Definers) => O,
 ) {
   return getSchema({ arg, argList, flag });
 }
 
-function arg(input: ArgDefInput): ArgDef {
-  return { type: 'arg', ...input };
+function arg<T, I extends ArgDefInput<T>>(
+  input: I,
+): 'validator' extends keyof I
+  ? ArgDef<ReturnTypeOr<I['validator'], string>>
+  : ArgDef<string> {
+  return { type: 'arg', ...input } as any;
 }
 
 type ArgDefiner = typeof arg;
 
-function argList(input: ArgListDefInput): ArgListDef {
-  return { type: 'argList', ...input };
+function argList<T, I extends ArgListDefInput<T>>(
+  input: I,
+): 'validator' extends keyof I
+  ? ArgListDef<ReturnTypeOr<I['validator'], string>>
+  : ArgListDef<string> {
+  return { type: 'argList', ...input } as any;
 }
 
 type ArgListDefiner = typeof argList;
@@ -58,22 +75,27 @@ type FlagDefiner = typeof flag;
 
 // ==========
 
-type ExtractType<D extends OptionDef> = D['type'] extends 'flag'
+type ExtractType<D extends OptionDef<unknown>> = D extends FlagDef
   ? boolean
-  : D['type'] extends 'arg'
-  ? string
-  : D['type'] extends 'argList'
-  ? Array<string>
+  : D extends ArgDef<infer T>
+  ? T
+  : D extends ArgListDef<infer T>
+  ? Array<T>
   : never;
 
-type ParsedResult<O extends Record<string, OptionDef>> = {
+type ParsedResult<O extends Record<string, OptionDef<unknown>>> = {
   [K in keyof O]: ExtractType<O[K]>;
 };
 
 // ==========
 
 const _result = defineSchema(({ arg, flag }) => ({
-  foo: arg({ alias: 'f', typeLabel: '<foo>', description: 'Foo' }),
+  foo: arg({
+    alias: 'f',
+    typeLabel: '<foo>',
+    description: 'Foo',
+    validator: (x) => Number(x),
+  }),
   bar: argList({ alias: 'b', typeLabel: '<bar>', description: 'Bar' }),
   baz: flag({ alias: 'z', description: 'Baz' }),
 }));
